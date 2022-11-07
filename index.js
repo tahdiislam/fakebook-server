80dbd684b689dcd32ee96c3837b194a1cf37f564
tahdi-islam <tshdi60@gmail.com>
2022-11-07 15:54:14 +0600

    add jwt verification

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,26 +26,63 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+// jwt verify
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers?.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, emon) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.emon = emon;
+    next();
+  });
+}
+
 async function run() {
   try {
     const Posts = client.db("fakebook").collection("posts");
 
     // get all post of a specific user
-    app.get("/posts", async (req, res) => {
-      const email = req.query.email;
-      let query = {}
-      if(email){
-        query = { email: email };
+    app.get("/posts", verifyJWT, async (req, res) => {
+      const emon = req.emon;
+      if(emon.email !== req.query.email)
+      {
+        res.status(401).send({message: "unauthorized access"})
       }
+      const email = req.query.email;
+      const query = { email: email };
       const cursor = Posts.find(query);
       const storedPosts = await cursor.toArray();
       res.status(200).send(storedPosts);
     });
+
+    // give user access token
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5000",
+      });
+      res.status(200).send({ token });
+    });
+
     // create a new user
     app.post("/posts", async (req, res) => {
       const post = req.body;
       const result = await Posts.insertOne(post);
       res.status(201).send(result);
+    });
+
+    // delete certain post
+    app.delete("/posts/:id", async (req, res) => {
+      // console.log(req.params.id);
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await Posts.deleteOne(query);
+      res.status(200).send(result);
     });
   } finally {
   }
